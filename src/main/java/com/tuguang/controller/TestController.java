a8505914b9842e216985355e7a0612050ee56578
Tu-guang <1626406673@qq.com>
2023-05-07 10:01:48 +0800

    readme

--- src/main/java/com/tuguang/controller/TestController.java
+++ src/main/java/com/tuguang/controller/TestController.java
@@ -1,8 +1,14 @@
 package com.tuguang.controller;
 
+import lombok.Data;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Data
 public class TestController {
-
+    @GetMapping("/hello")
+    public String Hello(){
+        return "Hello SpringBoot!";
+    }
 }
