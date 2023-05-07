ca418651e8dad126a725f90c34fb62f272f174ff
Tu-guang <1626406673@qq.com>
2023-05-07 08:55:31 +0800

    first commit

---
+++ src/main/java/com/tuguang/TuguangApplication.java
@@ -0,0 +1,13 @@
+package com.tuguang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuguangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuguangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuguang/TuguangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuguang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuguangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
